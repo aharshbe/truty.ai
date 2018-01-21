8e68f137d9cbf4f0ee30c02d20fb21e2853681c6
Austin Harshberger <a.catawampus@gmail.com>
2018-01-21 07:35:03 -0800

    adding fixing ai and blockchain

--- ProjectAI/main.py
+++ ProjectAI/main.py
@@ -8,6 +8,10 @@ import random
 import excScore
 import captLog
 import pldChoices
+import blockchain
+import datetime
+import hashlib
+import time
 
 #Creating data array
 print("****************************************  **************")
@@ -23,8 +27,161 @@ print("*********  ****  ***************************************")
 print("********************************************************")
 print("********************************************************")
 
+#Creating classes for blockchain
+class Message:
+        def __init__(self, data):
+                self.hash = None
+                self.prev_hash = None
+                self.timestamp = time.time()
+                self.size = len(data.encode('utf-8'))   # length in bytes
+                self.data = data
+                self.payload_hash = self._hash_payload()
+
+        def _hash_payload(self):
+                return hashlib.sha256(bytearray(str(self.timestamp) + str(self.data), "utf-8")).hexdigest()
+
+        def _hash_message(self):
+                return hashlib.sha256(bytearray(str(self.prev_hash) + self.payload_hash, "utf-8")).hexdigest()
+
+        def link(self, message):
+                """ Link the message to the previous one via hashes."""
+                self.prev_hash = message.hash
+
+        def seal(self):
+                """ Get the message hash. """
+                self.hash = self._hash_message()
+
+        def validate(self):
+                """ Check whether the message is valid or not. """
+                if self.payload_hash != self._hash_payload():
+                        raise InvalidMessage("Invalid payload hash in message: " + str(self))
+                if self.hash != self._hash_message():
+                        raise InvalidMessage("Invalid message hash in message: " + str(self))
+
+        def __repr__(self):
+                return 'Message<hash: {}, prev_hash: {}, data: {}>'.format(
+                        self.hash, self.prev_hash, self.data[:20]
+                )
 
 
+class Block:
+        def __init__(self, *args):
+                self.messages = []
+                self.timestamp = None
+                self.prev_hash = None
+                self.hash = None
+                if args:
+                        for arg in args:
+                                self.add_message(arg)
+
+        def _hash_block(self):
+                return hashlib.sha256(bytearray(str(self.prev_hash) + str(self.timestamp) + self.messages[-1].hash, "utf-8")).hexdigest()
+
+        def add_message(self, message):
+                if len(self.messages) > 0:
+                        message.link(self.messages[-1])
+                message.seal()
+                message.validate()
+                self.messages.append(message)
+
+        def link(self, block):
+                """ The block hash only incorporates the head message hash
+                        which then transitively includes all prior hashes.
+                """
+                self.prev_hash = block.hash
+
+        def seal(self):
+                self.timestamp = time.time()
+                self.hash = self._hash_block()
+
+        def validate(self):
+                """ Validates each message hash, then chain integrity, then the block hash.
+                        Calls each message's validate() method.
+
+                        If a message fails validation, this method captures the exception and
+                        throws InvalidBlock since an invalid message invalidates the whole block.
+                """
+                for i, msg in enumerate(self.messages):
+                        try:
+                                msg.validate()
+                                if i > 0 and msg.prev_hash != self.messages[i - 1].hash:
+                                        raise InvalidBlock(
+                                            "Invalid block: Message #{} has invalid message link in block: {}".format(i, str(self)))
+                        except InvalidMessage as ex:
+                                raise InvalidBlock("Invalid block: Message #{} failed validation: {}. In block: {}".format(
+                                        i, str(ex), str(self))
+                                )
+
+        def __repr__(self):
+                return 'Block<hash: {}, prev_hash: {}, messages: {}, time: {}>'.format(
+                        self.hash, self.prev_hash, self.messages, self.timestamp
+                )
+
+
+class SimpleChain:
+        def __init__(self):
+                self.chain = []
+
+        def add_block(self, block):
+                """ Add a block if valid."""
+                if len(self.chain) > 0:
+                        block.prev_hash = self.chain[-1].hash
+                block.seal()
+                block.validate()
+                self.chain.append(block)
+
+        def validate(self):
+                """ Validates each block, in order.
+                        An invalid block invalidates the chain.
+                """
+                for i, block in enumerate(self.chain):
+                        try:
+                                block.validate()
+                        except InvalidBlock as exc:
+                                raise InvalidBlockchain(
+                                    "Invalid blockchain at block number {} caused by: {}".format(i, str(exc)))
+                return True
+
+        def __repr__(self):
+                return 'SimpleChain<blocks: {}>'.format(len(self.chain))
+
+
+class InvalidMessage(Exception):
+        def __init__(self, *args, **kwargs):
+                Exception.__init__(self, *args, **kwargs)
+
+
+class InvalidBlock(Exception):
+        def __init__(self, *args, **kwargs):
+                Exception.__init__(self, *args, **kwargs)
+
+
+class InvalidBlockchain(Exception):
+        def __init__(self, *args, **kwargs):
+                Exception.__init__(self, *args, **kwargs)
+
+#blockchain function
+def blockchain_fun():
+    if __name__ == "__main__":
+                    chain = SimpleChain()
+                    block = Block()
+                    index = 0
+                    print("Please, enter your PLD review of " + name + " for the blockchain: ")
+                    block.add_message(Message(input()))
+                    if len(block.messages) > 0:
+                            chain.add_block(block)
+                            block = Block()
+                            if len(chain.chain) > 0:
+                                    try: print(chain.chain[index].messages)
+                                    except: print("The block does not exist at this location.")
+                            print("*******************")
+                            print("Current chain:")
+                            print("******************")
+                            for b in chain.chain:
+                                print(b)
+                            print("----------------")
+                            if chain.validate(): print("Integrity validated.")
+                    else: print("Block is empty, try adding some messages")
 
 #Seeindg data with 0's
 num_people = 6
@@ -41,8 +198,6 @@ for i in range(num_people):
     print("           Student {i} added as {name}".format(**locals()))
     print("**********************************************")
     print("                                              ")
-
-
 
 #Print all studnets to verify
 print("Current Student Pool:")
@@ -89,16 +244,25 @@ error = 0
 sum = 0
 for leadChoice in range(num_people):
     name = data[choices[0]][0]
-    print("Student " + str(leadChoice) + ", enter your review for Lead: " + name)
-    sum += int(input())
+    print("Enter Lead review for AI: ")
+    index = int(input())
+    #calling blockchain
+    blockchain_fun()
+    sum += index
+   
 data[choices[0]][1] = (data[choices[0]][1] + sum)/(data[choices[0]][1]+1)
 error += sum;
 sum = 0
 print("****************Now Review******************")
 for assitantChoice in range(num_people):
     name = data[choices[1]][0]
-    print("Student " + str(assitantChoice) + ", enter your review for Assitant: " + name)
-    sum+= int(input())
+    print("Enter Assistant review for AI: ")
+    index = int(input())
+    #calling blockchain
+    blockchain_fun()
+    sum+= index
+    
+
 data[choices[1]][1] = (data[choices[1]][1] + sum)/(data[choices[1]][1]+1)
 error += sum
 
